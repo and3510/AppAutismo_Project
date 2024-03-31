0c65fd3123a705f4bcdc8525caae5f881369a8f4
anderson <and.dovale@gmail.com>
2024-03-30 23:58:09 -0300

    organization of structure

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,47 +1,56 @@
+// Importar Componentes
 import {Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 
+// Importar Componentes da Pasta 
 import { style } from './styles';
 import Participantes from '../../components/participantes';
 
 export default function Home() {
 
-    const participantes = ['Gustavo', 'Vini', 'Diego', 'Anderson', 'Vinicius', 'Daniel', 'Joao'];
+  // Lista de Usuários
+  const participantes = ['Gustavo', 'Vini', 'Diego', 'Anderson', 'Vinicius', 'Daniel', 'Joao'];
 
-    function handleParticipantAdd() {
-      if(participantes.includes("Gustavo")) {
+  // Função Adicionar o Nome do Usuário
+  function handleParticipantAdd() {
+    if(participantes.includes("Gustavo")) {
         return Alert.alert(" Participante ja existe")
+    }
+  }
+
+  // Função Remover o Nome do Usuário
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover",`remover participante ${name}?`,[
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert("Deletado")
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
       }
-    }
+    ])
+    console.log(`Voce clicou em remove o aluno ${name}`);
+  }
 
-    function handleParticipantRemove(name: string) {
-      Alert.alert("Remover",`remover participante ${name}?`, [
-        {
-          text: 'Sim',
-          onPress: () => Alert.alert("Deletado")
+  return (
+    // Container do Aplicativo
+    <View style={style.container}>
+      {/* Titulo da Página  */}
+      <Text style={style.texto}>
+        Cadastro
+      </Text>
+      {/* Categoria do Nome do Usuário */}
+      <Text style={style.descricao}>
+        Username:
+      </Text>
 
-        },
-        {
-          text: 'Não',
-          style: 'cancel'
-        }
-      ])
-      console.log(`Voce clicou em remove o aluno ${name}`);
-    }
-
-    return (
-      <View style={style.container}>
-        <Text style={style.texto}>
-          Cadastro
-        </Text>
-        <Text style={style.descricao}>
-          Username:
-        </Text>
-        <View style={style.form}>
+      {/* Placeholder e Botão para Adicionar o nome do Usuário  */}
+      <View style={style.form}>
         <TextInput style=
           {style.input}
           placeholder="Nome do Jogador"
           placeholderTextColor="#6b6b6b"
-         />
+        />
 
         <TouchableOpacity style={style.button} onPress={handleParticipantAdd} >
           <Text style={style.buttontext}>
@@ -49,28 +58,32 @@ export default function Home() {
           </Text>
           
         </TouchableOpacity>
-        </View>
+      </View>
 
-        <Text style={style.descricao}> 
-          Idade:
-        </Text>
-        <View style={style.form}>
-         <TextInput style={style.input2} 
-         placeholder="Idade do Jogador"
-         placeholderTextColor="#6b6b6b"
-         keyboardType='numeric'
-         />
+
+      {/* Categoria do Idade do Usuário */}
+      <Text style={style.descricao}> 
+        Idade:
+      </Text>
+
+      {/* Placeholder e Botão para Adicionar a idade do Usuário  */}
+      <View style={style.form}>
+        <TextInput style={style.input} 
+        placeholder="Idade do Jogador"
+        placeholderTextColor="#6b6b6b"
+        keyboardType='numeric'
+        />
 
         <TouchableOpacity style={style.button} onPress={handleParticipantAdd} >
           <Text style={style.buttontext}>
           +
           </Text>
-          
         </TouchableOpacity>
-          
-        </View>
+        
+      </View>
 
-        <FlatList
+      {/* Listar os Usuários */}
+      <FlatList
         data={participantes}
         keyExtractor={item => item}
         renderItem={({ item }) => (
@@ -79,21 +92,15 @@ export default function Home() {
           name={item}
           onRemove={() => handleParticipantRemove(item)}
           />
-        )
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={style.listEmptyText}>
+              Ninguém Aqui
+          </Text>
+        )}
+      />
       
-      }
-      showsVerticalScrollIndicator={false}
-      ListEmptyComponent={() => (
-        <Text style={style.listEmptyText}>
-            Ninguém Aqui
-        </Text>
-      )}
-        
-
-        
-        />
-        
-
-      </View>
-    ); 
-  }
+    </View>
+  ); 
+}
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,72 +1,56 @@
 import { StyleSheet } from "react-native";
 
-export const style = StyleSheet.create(
-    {
-    container:{
-        flex: 1,
-        backgroundColor: '#131016',
-        padding: 24
-    },
-
-    texto:{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 65,
-        marginBottom: 20
-    },
-    descricao: {
-        color: '#6B6B6B',
-        fontSize: 22,
-        padding: 3
-    },
-    input: {
-        flex: 1,
-        height: 56,
-        backgroundColor: '#1f1e25',
-        borderRadius: 5,
-        color: '#fff',
-        padding: 16,
-        fontSize: 16,
-        marginRight: 5
-    },
-    input2: {
-        flex: 1,
-        height: 56,
-        backgroundColor: '#1f1e25',
-        borderRadius: 5,
-        color: '#fff',
-        padding: 16,
-        fontSize: 16,
-        marginRight: 5
-        
-    },
-    buttontext: {
-        color: '#fff',
-        fontSize: 24
-    },
-    button: {
-        width: 56,
-        height: 56,
-        borderRadius: 5,
-        backgroundColor: '#31cf67',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    form: {
-        width: '100%',
-        flexDirection: 'row'
-    },
-    listEmptyText: {
-        padding: 20,
-        color: "#fff",
-        fontSize: 20,
-        textAlign: "center"
-    }
-
+export const style = StyleSheet.create({
     
+container:{
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+},
 
-    }
+texto:{
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 65,
+    marginBottom: 20
+},
+descricao: {
+    color: '#6B6B6B',
+    fontSize: 22,
+    padding: 3
+},
+input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fff',
+    padding: 16,
+    fontSize: 16,
+    marginRight: 5
+},
+buttontext: {
+    color: '#fff',
+    fontSize: 24
+},
+button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31cf67',
+    alignItems: 'center',
+    justifyContent: 'center'
+},
+form: {
+    width: '100%',
+    flexDirection: 'row'
+},
+listEmptyText: {
+    padding: 20,
+    color: "#fff",
+    fontSize: 20,
+    textAlign: "center"
+}
 
-
-)
+})
